109e6e29fa1f1837de5c332b9e4623e3599a591b
Bryan Barton <39375032+bryanbarton525@users.noreply.github.com>
2026-04-08 20:48:19 -0400

    add pkg/strutil/strutil.go

---
+++ pkg/strutil/strutil.go
@@ -0,0 +1,11 @@
+package strutil
+
+// ReverseString reverses the Unicode characters of the input string s.
+// It correctly handles multi-byte UTF-8 characters by operating on runes.
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
